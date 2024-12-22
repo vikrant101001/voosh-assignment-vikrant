19f0de65166a19baf7308b59e9f66dcdbec1af9d
vikrant101001 <vikrant.th2013@gmail.com>
2024-12-22 23:36:45 +0530

    vercel v3

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,38 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Enable CORS
-  app.enableCors();
+  // Enable CORS with specific options
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Music Library API')
+    .setTitle('Music Library API- Voosh_assignment-vikrant')
     .setDescription('REST API for managing music library')
     .setVersion('1.0')
     .addBearerAuth()
+    .addServer('/')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Music Library API',
+    customfavIcon: 'https://swagger.io/favicon-32x32.png',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
-  // Use process.env.PORT for Vercel
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap(); 
